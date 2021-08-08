67df332bb6cb465a82de979118e7cd503e5327c1
philip <cz.doublep@gmail.com>
2021-08-08 17:48:01 +0900

    Modify for rizon

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -20,11 +20,9 @@ export default class Footer extends React.Component {
         return (
             <div>
                 <Navbar color="light" light expand="md" fixed="bottom" id="footer" className="d-none d-md-flex">
-                    <span className="text-muted"><a href="https://raw.githubusercontent.com/forbole/big_dipper/master/LICENSE" target="_blank"><T>navbar.license</T></a> &copy;2018-{moment().format('YYYY')}. </span>
+                <span className="text-muted"><a href="https://raw.githubusercontent.com/doublep-Philip/coowscan/rizon/LICENSE" target="_blank"><T>navbar.license</T></a> &copy;{moment().format('YYYY')}. </span>
                     <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href="https://www.github.com/forbole/big_dipper" target="_blank"><i className="fab fa-github"></i> <T>navbar.forkMe</T></NavLink>
-                        </NavItem>
+                        <NavItem></NavItem>
                     </Nav>
                 </Navbar>
                 <Navbar color="light" light fixed="bottom" className="d-block d-md-none mobile-menu">
--- imports/ui/home/Home.jsx
+++ imports/ui/home/Home.jsx
@@ -18,8 +18,8 @@ export default class Home extends Component{
     render() {
         return <div id="home">
             <Helmet>
-                <title>Big Dipper | Cosmos Explorer presented by Forbole</title>
-                <meta name="description" content="Cosmos is a decentralized network of independent parallel blockchains, each powered by BFT consensus algorithms like Tendermint consensus." />
+                <title>Coow Scan Explorer</title>
+                <meta name="description" content="RIZON is a decentralized network of independent parallel blockchains, each powered by BFT consensus algorithms like Tendermint consensus." />
             </Helmet>
             <ChainInfo/>
             <Consensus />
